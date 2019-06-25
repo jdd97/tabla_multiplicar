51223e6a22fcfeab92e300692f642f7513a94a7f
unknown <daniolimpia97@gmail.com>
2019-06-24 20:56:11 -0400

    first commit

---
+++ 01-hola-mundo/app.js
@@ -0,0 +1,5 @@
+let nombre ='Ana' ; 
+
+
+console.log('Hola ' + nombre );
+console.log(`Hola ${ nombre }` );
---
+++ 01-hola-mundo/app2.js
@@ -0,0 +1,11 @@
+function saludar ( nombre ) {
+    let mensaje = ` Hola ${ nombre}`; 
+
+    return mensaje; 
+}
+
+let saludo = saludar ('Ana'); 
+
+
+
+console.log(saludo);
